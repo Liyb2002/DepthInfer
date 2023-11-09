c578bd7c39a47b5cbc6ed5d893b97d901ac945ac
Yuanbo Li <yl4092@columbia.edu>
2023-11-09 13:11:36 -0500

    init

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+import pandas as pd
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+path = "/Users/yuanboli/Desktop/DS/Image/0.png"
+img = cv2.imread(path)
+print(img.shape)
+
+# filename = 'savedImage.jpg'
+# cv2.imwrite(filename, img) 
+
+path = "/Users/yuanboli/Desktop/DS/Image"
+img_filelist = []
+for root, dirs, files in os.walk(path):
+    for file in files:
+        if file == ".DS_Store":
+            continue
+        img_filelist.append(os.path.join(root, file))
+
+
+img_filelist.sort()
+
+path = "/Users/yuanboli/Desktop/DS/DepthMap"
+depth_filelist = []
+for root, dirs, files in os.walk(path):
+    for file in files:
+        depth_filelist.append(os.path.join(root, file))
+
+depth_filelist.sort()
+
+
+
+data = {
+    "image": [x for x in img_filelist],
+    "depth": [x for x in depth_filelist],
+}
+df = pd.DataFrame(data)
+
+df = df.sample(frac=1, random_state=42)
+
+
+HEIGHT = 256
+WIDTH = 256
+LR = 0.0002
+EPOCHS = 30
+BATCH_SIZE = 32
