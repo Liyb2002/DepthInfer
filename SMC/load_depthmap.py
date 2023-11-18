ae9f9fce64bc984d84004dd81e1a2816cfb5a85a
Yuanbo Li <yl4092@columbia.edu>
2023-11-17 20:58:22 -0500

    vis

--- SMC/load_depthmap.py
+++ SMC/load_depthmap.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 
 import config
 
 detph_map_path = './depthMap/predict.png'
+file_name = './three/sample_points.json'
 depth_map = cv2.imread(detph_map_path)
 
 sum_depthMap = np.sum(depth_map, axis=2, keepdims=True)
@@ -20,10 +22,28 @@ for i in range (0, sum_depthMap.shape[0]):
         if sum_depthMap[i][j] >720:
           continue
         else:
-          dist = (sum_depthMap[i][j]- 540) * 0.05
-          sample_point = cam.get_position(np.array([i,j]), dist)
+          dist = (sum_depthMap[i][j]-540) * 0.05
+          pos_x = j * (5.0 / 256.0)
+          pos_y = 5.0 - (i * (5.0 / 256.0))
+          sample_point = np.array([pos_x, pos_y, dist[0]])
+          # sample_point = cam.get_position(np.array([i,j]), dist)
           sample_points.append(sample_point)
 
 
+def output_sample_pts (sample_points, file_name):
+  result = []  
+  for pt in sample_points:
+      data = {'pt':
+          {
+          'x': float(pt[0]),
+          'y': float(pt[1]),
+          'z': float(pt[2]),
+          }
+          }
+      result.append(data)
+
+  with open(file_name, 'w') as f:
+      json.dump(result, f, indent=2)
 
 
+output_sample_pts(sample_points, file_name)
